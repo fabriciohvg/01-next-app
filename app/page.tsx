ca9895c5209f4f943beb0ffdad2037afe445dffd
Fabrício Guimarães <fabriciohenrique.eng@gmail.com>
2025-09-02 10:36:05 -0300

    2nd commit adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-1 max-w-lg mx-auto gap-4 p-4">
+    <div className="grid max-w-lg mx-auto gap-4 p-4">
       <h1 className="text-2xl">Cadastro de novos membros</h1>
       <p>
         Preencha o formulário a seguir com os seus dados para tornar-se membro
